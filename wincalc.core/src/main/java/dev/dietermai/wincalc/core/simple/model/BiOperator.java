550a479d0ae947c3586ef643d168343d8d879b08
Dieter Mai <maidieter@googlemail.com>
2022-11-06 14:58:28 +0100

    Cleanup BiOperator

--- wincalc.core/src/main/java/dev/dietermai/wincalc/core/simple/model/BiOperator.java
+++ wincalc.core/src/main/java/dev/dietermai/wincalc/core/simple/model/BiOperator.java
@@ -1,5 +1,8 @@
 package dev.dietermai.wincalc.core.simple.model;
 
+/**
+ * The binary operators supported by the simple calculator
+ */
 public enum BiOperator {
 	plus, minus, multiply, divide;
 }
