e7e904ead0d4be3333b886c12b2fd6be17407b73
Dieter Mai <maidieter@googlemail.com>
2022-11-06 15:11:00 +0100

    Cleanup

--- wincalc.core/src/main/java/dev/dietermai/wincalc/core/simple/model/Result.java
+++ wincalc.core/src/main/java/dev/dietermai/wincalc/core/simple/model/Result.java
@@ -2,22 +2,27 @@ package dev.dietermai.wincalc.core.simple.model;
 
 import java.math.BigDecimal;
 
+/**
+ * A Result is represents the result of an resolved expression. If the
+ * ResultType is OK, the the value is non null. If the result type is not OK,
+ * then the value can be ignored and my be null.
+ */
 public record Result(BigDecimal value, ResultType type) {
 	public static final Result ZERO = of(BigDecimal.ZERO);
-	
+
 	public static Result of(BigDecimal value) {
 		return new Result(value, ResultType.OK);
 	}
-	
+
 	public static Result of(ResultType type) {
 		return new Result(null, type);
 	}
-	
+
 	public boolean error() {
 		return type.error();
 	}
-	
-	public boolean isSuccess() {
+
+	public boolean isOK() {
 		return type.ok();
 	}
 }
--- wincalc.core/src/main/java/dev/dietermai/wincalc/core/simple/model/UnaryOperator.java
+++ wincalc.core/src/main/java/dev/dietermai/wincalc/core/simple/model/UnaryOperator.java
@@ -1,5 +1,11 @@
 package dev.dietermai.wincalc.core.simple.model;
 
+/**
+ * The unary operations supported by the simple calculator.
+ * 
+ * @author Dieter
+ *
+ */
 public enum UnaryOperator {
 	negate, percent, oneDivX, square, root;
 }
