7ddce98ca30570127c488ceaa865a27bb6bf078e
Dieter Mai <maidieter@googlemail.com>
2022-11-06 15:12:30 +0100

    Add comments to core module

--- wincalc.core/src/main/java/module-info.java
+++ wincalc.core/src/main/java/module-info.java
@@ -1,3 +1,8 @@
+
+/**
+ * The core module of the Windows calculator clone.
+ * Contains all the code required to calculate
+ */
 module wincalc.core {
 	exports dev.dietermai.wincalc.core.simple;
 	exports dev.dietermai.wincalc.core.simple.model;
